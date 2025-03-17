513712696b1b925f6df2be0b79a8dc1b1f45eaad
Detelina <detelinamarinova43@gmail.com>
2025-03-17 15:30:21 +0100

    hello

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,6 +1,3 @@
-resource "aws_lb" "Load Balancer"{
-    name = "aws_lb"
-    load_balancer_type = "application"
-    security_groups = aws
-
-}
+#resource "aws_lb" "Load Balancer"{
+ #   name = "aws_lb"
+   ###}
