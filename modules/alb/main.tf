ee06121d2b5fd55cb5b8c38fda0b52438540b7a8
Detelina <detelinamarinova43@gmail.com>
2025-03-19 14:41:04 +0100

    tf code to test in aws

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,3 +1,8 @@
-#resource "aws_lb" "Load Balancer"{
- #   name = "aws_lb"
-   ###}
+resource "aws_lb" "Load Balancer"{
+   name = "aws_lb"
+   load_balancer_type = "default"
+   internal = false
+   security_groups = [aws_security_group.lb_sg.id]
+   subnets = aws_subnet.PublicSubnet.id
+
+ }
