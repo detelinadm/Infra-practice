870c92da83edf39bfdd2d6eec2a050d735abc1ba
Detelina <detelinamarinova43@gmail.com>
2025-03-21 18:21:46 +0100

    remove alb sg

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,8 +1,8 @@
 resource "aws_lb" "aws_lb"{
    name = "aws_lb"
-   load_balancer_type = "default"
+   load_balancer_type = "application"
    internal = false
-   security_groups = [aws_security_group.lb_sg.id] # create
+   security_groups = [aws_security_group.lb_sg.id]
    subnets = var.public_subnet_ids # create
 
   tags = {
@@ -10,16 +10,17 @@ resource "aws_lb" "aws_lb"{
   }
  }
 
- resource "aws_security_group" "lb_sg" {
-   vpc_id = var.vpc_id # make ALB in the VPC
-   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
-  }
+ #resource "aws_security_group" "lb_sg" {
+#
+ #  vpc_id = var.vpc_id # make ALB in the VPC
+  # ingress {
+   # from_port   = 80
+    #to_port     = 80
+    #protocol    = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
+  #}
 
-  tags = {
-    Name = "ALB-SG"
-  }
-}
+  #tags = {
+   # Name = "ALB-SG"
+  #}
+#}
