99f59f1a7fa88bc8aace934086673d62afac9aa5
Detelina <detelinamarinova43@gmail.com>
2025-03-23 16:19:18 +0100

    .

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -45,7 +45,7 @@ resource "aws_lb_target_group_attachment" "instances" {
   for_each = var.target_instance_ids
 
   target_group_arn = aws_lb_target_group.aws_lb_tg.arn
-  target_id = each.value.id
+  target_id = each.value
   port = 80
 }
 #Listener
