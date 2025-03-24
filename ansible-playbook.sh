1a30f74f4d7f8af3c61c8d4e748c227f7680e4b3
Detelina <detelinamarinova43@gmail.com>
2025-03-24 13:39:06 +0100

    typos

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -3,11 +3,11 @@ set -o nounset -o pipefail -o errexit
 
 # Load all variables from .env and export them all for Ansible to read
 set -o allexport
-source "$(dirname "$0")/ansible-quickstart/.env"
+source "$(dirname "$0")/ansible_quickstart/.env"
 set +o allexport
 
 # Run database playbook
-ansible-playbook -i inventory ansible-quickstart/database.yml "$@"
+ansible-playbook -i inventory ansible_quickstart/database.yml "$@"
 
 # Run webserver playbook 
-ansible-playbook -i inventory ansible-quickstart/webserver.yml "$@"
+ansible-playbook -i inventory ansible_quickstart/webserver.yml "$@"
