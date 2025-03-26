2c0b7e7af8b78d84b7c4f7687533b26b20c09230
Detelina <detelinamarinova43@gmail.com>
2025-03-26 18:17:29 +0100

    test with jiadai ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,6 +3,7 @@
 # CLoudShell saves the files you have created but not installed packages and software
 # so you may need to run this after being inactive on CloudShell
 
-chmod +x install_ansible.sh
+echo "Installing Ansible"
+
 sudo dnf update -y
 sudo dnf install -y ansible
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -70,19 +70,20 @@ resource "aws_security_group" "web_sg" {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]    
   }
+  ingress {
+    description = "for HTTP flask"
+    from_port   = 5000 
+    to_port     = 5000 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
+   
  
     
 }
