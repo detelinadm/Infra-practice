3de0d7c6ed89e9715a1c68c777a2bba9ba763d2b
Detelina <detelinamarinova43@gmail.com>
2025-03-26 13:48:11 +0100

    cidr

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    security_groups = [aws_security_group.db_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
  
  
