601a221fd59dbc1e88d0bb1de29aea9059b45c37
Detelina <detelinamarinova43@gmail.com>
2025-03-26 11:06:17 +0100

    change variabel type

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -47,12 +47,12 @@ description = "ID of the VPC"
 
 variable "public_subnet1_id"{
    description = "ID of the public subnet 1"
-  type        = list(string)
+  type        = string
 }
 
 variable "public_subnet2_id"{
    description = "ID of the public subnet 2"
-  type        = list(string)
+  type        = string
 }
 variable "private_subnet_id" {
   description = "The ID of the private subnet"
