f12c09ef09c8eb6bc84cf2b666b2fb973d187ad4
Detelina <detelinamarinova43@gmail.com>
2025-03-26 19:03:37 +0100

    t

--- main.tf
+++ main.tf
@@ -44,11 +44,11 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id = var.public_subnet1_id.id
+  subnet_id = module.vpc.public_subnet1_id.id
 }
 
 resource "aws_route" "private_nat" {
-  route_table_id = var.route_table.PrivateRT
+  route_table_id = module.vpc.route_table_id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
